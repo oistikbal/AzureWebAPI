af82df152a6e3bba7b24079f91dc945dd1b2c85b
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2025-03-15 16:49:38 +0300

    Update DB

--- Database/ApplicationDBContext.cs
+++ Database/ApplicationDBContext.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace AzureWebAPI.Database
 {
@@ -8,9 +9,19 @@ namespace AzureWebAPI.Database
 
     public class ApplicationDBContext : IdentityDbContext<User, IdentityRole, string>
     {
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
-    : base(options)
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
+        }
+    }
+
+    public class ApplicationDBContextFactory : IDesignTimeDbContextFactory<ApplicationDBContext>
+    {
+
+        ApplicationDBContext IDesignTimeDbContextFactory<ApplicationDBContext>.CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDBContext>();
+            optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING"));
+            return new ApplicationDBContext(optionsBuilder.Options);
         }
     }
 }
